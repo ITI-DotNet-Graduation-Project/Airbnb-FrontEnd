3686c240711d299d6de5a9290553272cb7def53d
EmanElsayed2002 <emoelsayed2192002@gmail.com>
2025-04-29 04:52:23 +0300

    edit nterception

--- src/app/Interceptor/interceptor.ts
+++ src/app/Interceptor/interceptor.ts
@@ -6,24 +6,42 @@ import {
   HttpEvent,
   HttpInterceptor,
 } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { catchError, Observable, throwError } from 'rxjs';
 import { AuthService } from '../AuthService/auth-service.service';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
   constructor(private authService: AuthService) {}
 
-  intercept(request: HttpRequest<unknown>, next: HttpHandler) {
+  intercept(
+    request: HttpRequest<unknown>,
+    next: HttpHandler
+  ): Observable<HttpEvent<unknown>> {
     const token = this.authService.getToken();
+
+    if (
+      request.url.includes('/Auth/login') ||
+      request.url.includes('/Auth/register')
+    ) {
+      return next.handle(request);
+    }
 
     if (token) {
       request = request.clone({
         setHeaders: {
           Authorization: `Bearer ${token}`,
         },
+        withCredentials: true,
       });
     }
 
-    return next.handle(request);
+    return next.handle(request).pipe(
+      catchError((error) => {
+        if (error.status === 401) {
+          this.authService.logout();
+        }
+        return throwError(error);
+      })
+    );
   }
 }
